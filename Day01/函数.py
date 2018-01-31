e27b191c16f3c05d47585096acf78c1aafb1c926
chenghao <375130788@qq.com>
2018-01-31 17:34:03 +0800

    2018-1-31

--- Day01/函数.py
+++ Day01/函数.py
@@ -1,5 +1,11 @@
 #函数
 def sum(index1,index2):
+    """
+    这个是函数哦
+    :param index1:  index1
+    :param index2:   index2
+    :return:  返回值
+    """
     con=index1+index2
     print("%d +%d=%d"%(index1,index2,con))
     return con
